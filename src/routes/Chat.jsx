b5063ad672e2ed434296881a40d19c218277dfec
Ivanna Novak <ivanna1998.novak@gmail.com>
2020-02-27 13:48:24 +0200

    add loader to chat component

--- src/routes/Chat.jsx
+++ src/routes/Chat.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import {
   Switch, Route, Redirect,
 } from 'react-router-dom';
-import { Header, Icon } from 'semantic-ui-react';
+import {
+  Header, Icon, Loader, Dimmer,
+} from 'semantic-ui-react';
 import { SocketApi } from '../services/socketApi';
 import { Channels } from './channels/Channels';
 import { UserInfo } from '../components/userInfo';
 
 export const Chat = ({ history, match }) => {
   const token = localStorage.getItem('auth');
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
+    setLoading(true);
     if (token && !SocketApi.io) {
       SocketApi.connect(token);
+      setLoading(false);
     } else {
       history.push('/login');
     }
@@ -20,16 +25,22 @@ export const Chat = ({ history, match }) => {
 
   return (
     <div className="w-100 h-100">
+
       <Header as="h1" block>
         <Icon name="rocket" size="massive" />
         <Header.Content>Messenger</Header.Content>
       </Header>
       <section className="chatWrapper">
-        <Switch>
-          <Route path={`${match.path}/channels`} component={Channels} />
-          <Route exact path="/info" component={UserInfo} />
-          <Redirect to={`${match.path}/channels`} />
-        </Switch>
+        {
+          loading ? (<Dimmer><Loader /></Dimmer>) : (
+            <Switch>
+              <Route path={`${match.path}/channels`} component={Channels} />
+              <Route exact path="/info" component={UserInfo} />
+              <Redirect to={`${match.path}/channels`} />
+            </Switch>
+          )
+        }
+
       </section>
 
     </div>
