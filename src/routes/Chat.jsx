654353b6a8c78bf324c280f1114ded057d805043
Ivanna Novak <ivanna1998.novak@gmail.com>
2020-03-04 16:55:14 +0200

    add logout

--- src/routes/Chat.jsx
+++ src/routes/Chat.jsx
@@ -3,7 +3,7 @@ import {
   Switch, Route, Redirect,
 } from 'react-router-dom';
 import {
-  Header, Icon, Loader, Dimmer,
+  Header, Icon, Loader, Dimmer, Button,
 } from 'semantic-ui-react';
 import Axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
@@ -16,35 +16,47 @@ export const Chat = ({ history, match }) => {
   const token = localStorage.getItem('auth');
   const [loading, setLoading] = React.useState(true);
 
+  const onClick = React.useCallback(() => {
+    localStorage.removeItem('auth');
+    history.push('/login');
+  }, [history]);
+
   const fetchChannel = React.useCallback(async (channelId) => {
     const { data } = await Axios.get(`http://localhost:3002/channels/${channelId}`);
     return data.channel;
   }, []);
 
   React.useEffect(() => {
+    if (SocketApi.io) {
+      SocketApi.io.on('addedMessChannel', async ({ addedMess, channelId }) => {
+        const channel = await fetchChannel(channelId);
+        toast(<Notification channel={channel} msg={addedMess} />);
+      });
+      return () => SocketApi.io.off('addedMessChannel');
+    }
+  }, [fetchChannel]);
+
+  React.useEffect(() => {
     setLoading(true);
     if (token && !SocketApi.io) {
       SocketApi.connect(token);
       setLoading(false);
-    } else if (!token) {
+      return () => SocketApi.io.disconnect();
+    } if (!token) {
       history.push('/login');
     }
   }, [token, history]);
 
-  React.useEffect(() => {
-    SocketApi.io.on('addedMessChannel', async ({ addedMess, channelId }) => {
-      const channel = await fetchChannel(channelId);
-      toast(<Notification channel={channel} msg={addedMess} />);
-    });
-    return () => SocketApi.io.off('addedMessChannel');
-  }, [fetchChannel]);
 
   return (
     <div className="w-100 h-100">
 
-      <Header as="h1" block>
+      <Header as="h1" block className="d-flex w-100">
         <Icon name="rocket" size="massive" />
-        <Header.Content>Messenger</Header.Content>
+        <Header.Content className="d-flex justify-content-between w-100">
+          <div>Messenger</div>
+          <Button onClick={onClick}>LogOut</Button>
+        </Header.Content>
       </Header>
       <section className="chatWrapper">
         {
